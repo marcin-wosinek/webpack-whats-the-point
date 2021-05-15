8bc3001e21b8406c198688ec5338f3287ae580ad
Marcin Wosinek <marcin@wosinek.eu>
2021-05-15 09:56:15 +0100

    loading npm scripts

--- src/hello-world.js
+++ src/hello-world.js
@@ -1,3 +1,5 @@
+import range from "../node_modules/underscore/modules/range.js";
+
 const element = document.getElementById("echo");
 
-element.innerHTML = "hello world";
+element.innerHTML = range(0, 10).join(", ");
